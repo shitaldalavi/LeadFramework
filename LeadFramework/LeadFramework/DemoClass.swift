9725e4bd787adbd5791e0ade3532aec0830dbcc4
shital.dalavi <shital.dalavi@nciportal.com>
2021-08-05 13:14:27 +0530

    First commit

---
+++ LeadFramework/LeadFramework/DemoClass.swift
@@ -0,0 +1,21 @@
+//
+//  DemoClass.swift
+//  LeadFramework
+//
+//  Created by admin on 05/08/21.
+//
+
+import Foundation
+
+public class DemoClass{
+    private init()
+    {
+        
+    }
+    public static func PrintData()->String
+    {
+        print("This is what actually wanteddddd.......")
+        let data = "Yesss worked...."
+       return data
+    }
+}
---
+++ LeadFramework/LeadFramework/LeadFramework.h
@@ -0,0 +1,18 @@
+//
+//  LeadFramework.h
+//  LeadFramework
+//
+//  Created by admin on 05/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeadFramework.
+FOUNDATION_EXPORT double LeadFrameworkVersionNumber;
+
+//! Project version string for LeadFramework.
+FOUNDATION_EXPORT const unsigned char LeadFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeadFramework/PublicHeader.h>
+
+
